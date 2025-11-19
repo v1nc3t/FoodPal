071457fade7a5624c4c81ffbec107e77f426004d
Hiroki <h.iwabuchi@student.tudelft.nl>
2025-11-19 21:33:24 +0100

    refactor FormalAmount and InformalAmount classes to implement equals and hashCode methods, and add necessary constructors and getters

--- commons/src/main/java/commons/InformalAmount.java
+++ commons/src/main/java/commons/InformalAmount.java
@@ -2,5 +2,32 @@ package commons;
 
 public class InformalAmount {
     private String description;
+
+    public InformalAmount(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        InformalAmount that = (InformalAmount) obj;
+        return description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return description.hashCode();
+    }
+    
     
 }
