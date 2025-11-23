7cb291a45ce8eaa0533136871a31d361a3217815
George Ciobanu <v.ciobanu@student.tudelft.nl>
2025-11-23 13:54:11 +0100

    feat(adding recipes): connected buttons and text fields. Adding preparations steps work now, can change order and delete a step. Added some server utils too.

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -1,17 +1,45 @@
 package client.utils;
 
+import commons.Ingredient;
+import commons.Recipe;
 import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.client.ClientBuilder;
 
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.GenericType;
 import org.glassfish.jersey.client.ClientConfig;
 
 import java.net.ConnectException;
+import java.util.List;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class ServerUtils implements IServerUtils {
 
     public static final String SERVER = "http://localhost:8080/";
+
+    public List<Ingredient> getIngredients() {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("api/ingredients")
+                .request(APPLICATION_JSON)
+                .get(new GenericType<List<Ingredient>>() {
+                });
+    }
+
+    public List<Recipe> getRecipes() {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("api/recipes")
+                .request(APPLICATION_JSON)
+                .get(new GenericType<List<Recipe>>() {
+                });
+    }
+
+    public Recipe addRecipe(Recipe recipe) {
+        return ClientBuilder.newClient(new ClientConfig())
+                .target(SERVER).path("api/recipes")
+                .request(APPLICATION_JSON)
+                .post(Entity.entity(recipe, APPLICATION_JSON), Recipe.class);
+    }
 
     public boolean isServerAvailable() {
         try {
