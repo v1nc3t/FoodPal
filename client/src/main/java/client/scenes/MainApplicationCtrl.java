26078f3f662497f93bf5f23d0fe1c00e297fd40f
Karen Martirosyan <k.h.martirosyan@student.tudelft.nl>
2025-11-24 23:30:42 +0100

    feat(ui): Moved FXML into constructor reformated the code using correct indents and javadoc

--- client/src/main/java/client/scenes/MainApplicationCtrl.java
+++ client/src/main/java/client/scenes/MainApplicationCtrl.java
@@ -1,6 +1,5 @@
 package client.scenes;
 
-
 import client.MyFXML;
 import jakarta.inject.Inject;
 import javafx.fxml.FXML;
@@ -11,37 +10,43 @@ import javafx.util.Pair;
 
 public class MainApplicationCtrl {
 
-  // This is the right pane(This pane will load different screens)
-  @FXML
-  private Pane contentPane;
+    /**
+     *   This is the right pane(This pane will load different screens)
+     */
+    @FXML
+    private Pane contentPane;
 
-  @FXML
-  private Button addButton;
+    @FXML
+    private Button addButton;
 
-  private MyFXML fxml;
+    private MyFXML fxml;
 
-  @Inject
-  public MainApplicationCtrl() {
-  }
+    @Inject
+    public MainApplicationCtrl(MyFXML fxml){
+        this.fxml =fxml;
+    }
 
-  public void initialize(MyFXML fxml) {
-    this.fxml = fxml;
-  }
+    /**
+     *   Loads Recipe panel
+     */
+    @FXML
+    private void addRecipe() {
+        Pair<AddRecipeCtrl, Parent> pair = fxml.load(AddRecipeCtrl.class,
+            "client", "scenes", "AddRecipePanel.fxml");
 
-  // Loads Recipe panel
-  @FXML
-  private void addRecipe() {
-    Pair<AddRecipeCtrl, Parent> pair = fxml.load(AddRecipeCtrl.class,
-        "client", "scenes", "AddRecipePanel.fxml");
+      /**
+       *  Injects the main ctrl into the add recipe ctrl
+        */
+        AddRecipeCtrl addRecipeCtrl = pair.getKey();
+        Parent addRecipeRoot = pair.getValue();
 
-    AddRecipeCtrl addRecipeCtrl = pair.getKey();    // injects the main ctrl into the add recipe ctrl
-    Parent addRecipeRoot = pair.getValue();
+        contentPane.getChildren().setAll(addRecipeRoot);
+    }
 
-    contentPane.getChildren().setAll(addRecipeRoot);
-  }
-
-  //This clears the current screen back to the main(blank for now)
-  public void showMainScreen(){
-    contentPane.getChildren().clear();
-  }
+    /**
+     *  This clears the current screen back to the main(blank for now)
+     */
+    public void showMainScreen(){
+        contentPane.getChildren().clear();
+    }
 }
