1893939e807a185cbcb7245a852d069aa7a47749
Hiroki <h.iwabuchi@student.tudelft.nl>
2025-11-24 16:34:06 +0100

    refactor Ingredient class to include nutrition values in constructors and update equals/hashCode methods

--- commons/src/main/java/commons/Ingredient.java
+++ commons/src/main/java/commons/Ingredient.java
@@ -7,10 +7,31 @@ public class Ingredient {
 
     private UUID id;
     private String name;
+    private NutritionValues nutritionValues;
 
-    public Ingredient(String name) {
+    /**
+     * Constructor for Ingredient class.
+     * The nutrition values are provided as separate parameters. And they are all for 100g.
+     * @param name the name of the ingredient
+     * @param nutritionValues the nutrition values of the ingredient
+     */
+    public Ingredient(String name, NutritionValues nutritionValues) {
         this.id = UUID.randomUUID();
         this.name = name;
+        this.nutritionValues = nutritionValues;
+    }
+
+    /**
+     * Don't use this constructor it's just for testing purposes!!!!!
+     * Constructor for Ingredient class. With id. 
+     * @param id
+     * @param name
+     * @param nutritionValues
+     */
+    public Ingredient(UUID id, String name, NutritionValues nutritionValues) {
+        this.id = id;
+        this.name = name;
+        this.nutritionValues = nutritionValues;
     }
 
     public UUID getId() {
@@ -21,14 +42,10 @@ public class Ingredient {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
         return "Ingredient{" +
-                "id=" + getId().toString() +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
     }
@@ -38,12 +55,12 @@ public class Ingredient {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         Ingredient that = (Ingredient) obj;
-        return id == that.id && name.equals(that.name);
+        return id == that.id && name.equals(that.name) && nutritionValues.equals(that.nutritionValues);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, nutritionValues);
     }
     
     
