0090b1eb6a42f6cab8ab87a671203df8dcbc2bdd
Lukas Zając <l.p.zajac@student.tudelft.nl>
2025-11-27 17:19:38 +0100

    feat(common): make Recipe an entity

--- commons/src/main/java/commons/Recipe.java
+++ commons/src/main/java/commons/Recipe.java
@@ -2,18 +2,21 @@ package commons;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
+@Entity
 public class Recipe {
-
-    private UUID id;
-    private String title;
-    private List<RecipeIngredient> ingredients;
-    private List<String> steps;
-    private int servingSize;
+    @Id
+    public UUID id;
+    public String title;
+    public List<RecipeIngredient> ingredients;
+    public List<String> steps;
+    public int servingSize;
     
     public Recipe(String title, List<RecipeIngredient> ingredients, List<String> steps, int servingSize) {
         this.id = UUID.randomUUID();
@@ -42,6 +45,10 @@ public class Recipe {
         this.ingredients = ingredients;
         this.steps = steps;
         this.servingSize = servingSize;
+    }
+
+    // an empty constructor for object mapper
+    public Recipe() {
     }
 
 
