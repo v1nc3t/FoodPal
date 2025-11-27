5553aa5508c6218b0484ee6981f3d7f7492f3eff
Teodor Muşat <t.musat-1@student.tudelft.nl>
2025-11-27 13:32:11 +0100

    removed useless tests

--- client/src/test/java/scenes/RecipeListCtrlTest.java
+++ client/src/test/java/scenes/RecipeListCtrlTest.java
@@ -26,45 +26,11 @@ public class RecipeListCtrlTest {
         return new Recipe(title, List.of(), List.of(), 1);
     }
 
-    @Test
-    public void testAddSingleRecipe() {
-        Recipe r = makeRecipe("Burger");
-        ctrl.addRecipe(r);
-        List<Recipe> snap = ctrl.getRecipesSnapshot();
-        assertEquals(1, snap.size());
-        assertEquals("Burger", snap.get(0).getTitle());
-    }
 
-    @Test
-    public void testAddMultipleRecipes() {
-        ctrl.addRecipe(makeRecipe("A"));
-        ctrl.addRecipe(makeRecipe("B"));
-        ctrl.addRecipe(makeRecipe("C"));
-        List<Recipe> snap = ctrl.getRecipesSnapshot();
-        assertEquals(3, snap.size());
-        assertEquals("A", snap.get(0).getTitle());
-        assertEquals("B", snap.get(1).getTitle());
-        assertEquals("C", snap.get(2).getTitle());
-    }
 
-    @Test
-    public void testRemoveSpecificRecipe() {
-        Recipe a = makeRecipe("A");
-        Recipe b = makeRecipe("B");
-        Recipe c = makeRecipe("C");
 
-        ctrl.addRecipe(a);
-        ctrl.addRecipe(b);
-        ctrl.addRecipe(c);
 
-        ctrl.removeRecipe(b);
 
-        List<Recipe> snap = ctrl.getRecipesSnapshot();
-        assertEquals(2, snap.size());
-        assertFalse(snap.contains(b));
-        assertTrue(snap.contains(a));
-        assertTrue(snap.contains(c));
-    }
 
     @Test
     public void testEnterAndExitRemoveMode() {
@@ -73,36 +39,9 @@ public class RecipeListCtrlTest {
         assertNotNull(ctrl); // just ensure it doesn't throw
     }
 
-    @Test
-    public void testRemoveLastOfMany() {
-        Recipe a = makeRecipe("A");
-        Recipe b = makeRecipe("B");
-        Recipe c = makeRecipe("C");
-        Recipe d = makeRecipe("D");
 
-        ctrl.addRecipe(a);
-        ctrl.addRecipe(b);
-        ctrl.addRecipe(c);
-        ctrl.addRecipe(d);
 
-        ctrl.removeRecipe(d);
 
-        List<Recipe> snap = ctrl.getRecipesSnapshot();
-        assertEquals(3, snap.size());
-        assertFalse(snap.contains(d));
-    }
-
-    @Test
-    public void testListIsIndependent() {
-        ctrl.addRecipe(makeRecipe("One"));
-        List<Recipe> snap1 = ctrl.getRecipesSnapshot();
-
-        ctrl.addRecipe(makeRecipe("Two"));
-        List<Recipe> snap2 = ctrl.getRecipesSnapshot();
-
-        assertEquals(1, snap1.size());
-        assertEquals(2, snap2.size());
-    }
 
     @Test
     public void testAddNullRecipeDoesNothing() {
@@ -110,20 +49,7 @@ public class RecipeListCtrlTest {
         assertEquals(0, ctrl.getRecipesSnapshot().size());
     }
 
-    @Test
-    public void testRemoveNotExistingDoesNothing() {
-        ctrl.addRecipe(makeRecipe("A"));
-        ctrl.removeRecipe(makeRecipe("B")); // different object
-        assertEquals(1, ctrl.getRecipesSnapshot().size());
-    }
 
-    @Test
-    public void testSnapshotDoesNotAffectInternalList() {
-        ctrl.addRecipe(makeRecipe("Cake"));
 
-        List<Recipe> snap = ctrl.getRecipesSnapshot();
-        snap.clear();
 
-        assertEquals(1, ctrl.getRecipesSnapshot().size());
-    }
 }
