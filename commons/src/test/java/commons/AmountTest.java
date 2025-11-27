0ab318afa94d5ea1fca776fc30570ce8544dba99
Hiroki <h.iwabuchi@student.tudelft.nl>
2025-11-27 13:20:07 +0100

    Revert "refactor(AmountTest): remove tests for FormalAmount and InformalAmount classes"
    This reverts commit 3466514985aa11d910e305ce6cecf1877312dee9.

--- commons/src/test/java/commons/AmountTest.java
+++ commons/src/test/java/commons/AmountTest.java
@@ -2,50 +2,138 @@ package commons;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AmountTest {
-    Amount formalAmount;
-    Amount informalAmount;
-
-    @BeforeEach
-    public void setUp() {
-        formalAmount = new Amount(2.0, Unit.CUP);
-        informalAmount = new Amount(1.0, "a pinch");
-    }
-
+    // Tests for FormalAmount subclass
     @Test
-    public void testformalAmountCreation() {
-        assertNotNull(formalAmount);
-    }
-
-    @Test
-    public void testinformalAmountCreation() {
-        assertNotNull(informalAmount);
+    public void testAmountSubclassCreation() {
+        Amount amount = new FormalAmount(100, Unit.GRAM);
+        assertNotNull(amount);
     }
 
     @Test
     public void testGetQuantity() {
-        assertEquals(2.0, formalAmount.getQuantity());
-        assertEquals(1.0, informalAmount.getQuantity());
+        FormalAmount amount = new FormalAmount(100, Unit.GRAM);
+        assertEquals(100, amount.getQuantity());
     }
 
     @Test
     public void testGetUnit() {
-        assertEquals(Unit.CUP, formalAmount.getUnit());
-        assertNull(informalAmount.getUnit());
+        FormalAmount amount = new FormalAmount(100, Unit.GRAM);
+        assertEquals(Unit.GRAM, amount.getUnit());
     }
 
     @Test
-    public void testGetDescription() {
-        assertNull(formalAmount.getDescription());
-        assertEquals("a pinch", informalAmount.getDescription());
+    public void testToString() {
+        FormalAmount amount = new FormalAmount(100, Unit.GRAM);
+        assertEquals("100.0 GRAM", amount.toString());
     }
 
     @Test
     public void testEqualsSameObject() {
-        assertEquals(formalAmount, formalAmount);
-        assertEquals(informalAmount, informalAmount);
-    }  
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        assertEquals(amount1, amount1);
+    }
+
+    @Test
+    public void testEqualsNull() {
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        assertFalse(amount1.equals(null));
+    }
+
+    @Test
+    public void testEqualsDifferentClass() {
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        String notAnAmount = "Not an Amount";
+        assertNotEquals(amount1, notAnAmount);
+    }
+
+    @Test
+    public void testEqualsSameValues() {
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        FormalAmount amount2 = new FormalAmount(100, Unit.GRAM);
+        assertEquals(amount1, amount2);
+    }
+
+    @Test
+    public void testNotEqualsDifferentValues() {
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        FormalAmount amount2 = new FormalAmount(200, Unit.GRAM);
+        assertNotEquals(amount1, amount2);
+    }
+
+    @Test
+    public void testNotEqualsDifferentUnits() {
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        FormalAmount amount2 = new FormalAmount(100, Unit.MILLILITER);
+        assertNotEquals(amount1, amount2);
+    }
+
+    @Test
+    public void testHashCode() {
+        FormalAmount amount1 = new FormalAmount(100, Unit.GRAM);
+        FormalAmount amount2 = new FormalAmount(100, Unit.GRAM);
+        assertEquals(amount1.hashCode(), amount2.hashCode());
+    }
+
+    
+    // Tests for InformalAmount subclass
+    @Test
+    public void testAmountSubclassCreation2() {
+        Amount amount = new InformalAmount("a pinch");
+        assertNotNull(amount);
+    }
+
+    @Test
+    public void testGetDescription() {
+        InformalAmount amount = new InformalAmount("a pinch");
+        assertEquals("a pinch", amount.getDescription());
+    }
+
+    @Test
+    public void testToString2() {
+        InformalAmount amount = new InformalAmount("a pinch");
+        assertEquals("a pinch", amount.toString());
+    } 
+
+    @Test
+    public void testEqualsSameObject2() {
+        InformalAmount amount1 = new InformalAmount("a pinch");
+        assertEquals(amount1, amount1);
+    }
+
+    @Test
+    public void testEqualsNull2() {
+        InformalAmount amount1 = new InformalAmount("a pinch");
+        assertFalse(amount1.equals(null));
+    }
+
+    @Test
+    public void testEqualsDifferentClass2() {
+        InformalAmount amount1 = new InformalAmount("a pinch");
+        String notAnAmount = "Not an Amount";
+        assertNotEquals(amount1, notAnAmount);
+    }
+
+    @Test
+    public void testEqualsSameValues2() {
+        InformalAmount amount1 = new InformalAmount("a pinch");
+        InformalAmount amount2 = new InformalAmount("a pinch");
+        assertEquals(amount1, amount2);
+    }
+
+    @Test
+    public void testNotEqualsDifferentValues2() {
+        InformalAmount amount1 = new InformalAmount("a pinch");
+        InformalAmount amount2 = new InformalAmount("a dash");
+        assertNotEquals(amount1, amount2);
+    }
+
+    @Test
+    public void testHashCode2() {
+        InformalAmount amount1 = new InformalAmount("a pinch");
+        InformalAmount amount2 = new InformalAmount("a pinch");
+        assertEquals(amount1.hashCode(), amount2.hashCode());
+    }    
 }
