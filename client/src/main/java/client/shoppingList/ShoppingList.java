3c8dc68e28644d0d5705f1159030fbe09b100ca6
Hiroki <h.iwabuchi@student.tudelft.nl>
2025-12-17 23:46:01 +0100

    refactor(ShoppingList); combine the 2 classes ShoppingListService and ShoppingList in one ShoppingList class

--- client/src/main/java/client/shoppingList/ShoppingList.java
+++ client/src/main/java/client/shoppingList/ShoppingList.java
@@ -2,6 +2,11 @@ package client.shoppingList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import commons.Amount;
+import commons.Recipe;
+import commons.RecipeIngredient;
 
 public class ShoppingList {
     private List<ShoppingListItem> items = new ArrayList<>();
@@ -12,6 +17,25 @@ public class ShoppingList {
 
     public List<ShoppingListItem> getItems() {
         return items;
+    }
+
+    public void clear() {
+        items.clear();
+    }
+
+    public void addManualItem(UUID ingredientId, Amount amount) {
+        items.add(new ShoppingListItem(ingredientId, amount, null));
+    }
+
+    public void addRecipeItems(Recipe recipe) {
+        List<RecipeIngredient> ingredients = recipe.getIngredients();
+        for (RecipeIngredient ingredient : ingredients) {
+            items.add(new ShoppingListItem(
+                ingredient.getIngredientRef(),
+                ingredient.getAmount(),
+                recipe.getId()
+            ));
+        }
     }
 
     @Override
@@ -32,9 +56,5 @@ public class ShoppingList {
     @Override
     public int hashCode() {
         return items.hashCode();
-    }
-
-    public void clear() {
-        items.clear();
     }
 }
