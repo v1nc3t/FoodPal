83573763f4e06232def4e1d2a5e5ef1858f21e24
George Ciobanu <v.ciobanu@student.tudelft.nl>
2026-01-18 14:59:07 +0100

    fix: on recipe toggle show main screen

--- client/src/main/java/client/scenes/MainApplicationCtrl.java
+++ client/src/main/java/client/scenes/MainApplicationCtrl.java
@@ -402,6 +402,7 @@ public class MainApplicationCtrl implements Internationalizable {
                 dutchFilter.setDisable(true);
                 germanFilter.setDisable(true);
             }
+            showMainScreen();
         });
     }
 
