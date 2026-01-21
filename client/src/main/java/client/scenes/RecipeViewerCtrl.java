685a453a89f0eadb1f8ebe9267a558332b23113c
George Ciobanu <v.ciobanu@student.tudelft.nl>
2026-01-21 22:41:29 +0100

    feat(printing): translated printing

--- client/src/main/java/client/scenes/RecipeViewerCtrl.java
+++ client/src/main/java/client/scenes/RecipeViewerCtrl.java
@@ -296,7 +296,10 @@ public class RecipeViewerCtrl implements Internationalizable {
                 currentRecipe,
                 id -> recipeManager
                         .getIngredient(new RecipeIngredient(id, null))
-                        .getName()
+                        .getName(),
+                ResourceBundle.getBundle(
+                        localeManager.getBundleName(), localeManager.getCurrentLocale()
+                )
         );
 
         TextFileExporter.save(
